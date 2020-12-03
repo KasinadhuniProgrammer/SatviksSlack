6b12c5c1d5f35d23a6f38e6158263f3c263a302e
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-03 18:24:19 -0500

    second code commit in this repo

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,10 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
+            <div></div>
 
         </div>
     ); 
 }
 
-export default Header; 
+export default Header;  
