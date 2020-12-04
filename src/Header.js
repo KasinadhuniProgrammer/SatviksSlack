527fb0e18a670558dda7ecfedaf714538e61a0ab
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-03 19:13:51 -0500

    prototyped header for this project

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'; 
-import './Header.css'; 
+import './Header.css';
 
+import { Avatar } from "@material-ui/core"; 
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
         <div className="header">
-            <div></div>
+            <div className="header__firstside">
+                <Avatar className="avatarheader"/>
+                <AccessTimeIcon className="accesstimeiconheader"/>
+
+            </div>
+            <div className="header__secondfirstside">
+                <SearchIcon className="searchiconheader"/>
+                <input placeholder="Search Satviks Slack" className="searchplaceholderheader"/>
+
+            </div>
+            <div className="header__secondside">
+                <HelpOutlineIcon className="helpoutlineiconheader"/>
+
+            </div>
 
         </div>
     ); 
