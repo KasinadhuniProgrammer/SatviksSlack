57da1b52f076ace9c25c9e7b22792c081b499697
prodSatvikK <rachitipidudraksha@gmail.com>
2020-12-17 09:36:49 -0500

    starting chat feature

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 
 import Header from './Header'; 
 import SideBar from './SideBar'; 
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <SideBar />
+      <Router>
+        <Header />
+        <div className="app__body">
+          <SideBar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>CHAT SCREEN</h1>
 
-      </div>
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
